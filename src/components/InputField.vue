de17ad6c9da6fc5933d04ccad40a4e484830ebee
yud0uhu <b2190280@photon.chitose.ac.jp>
2020-12-12 20:51:44 +0900

    初回コミット

---
+++ src/components/InputField.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            v-model="newItem"
+            solo
+            label="ToDo"
+            clearable
+          ></v-text-field>
+          <button
+            v-on:click="addItem"
+          >Add</button>
+        </v-col>
+
+      </v-row>
+    </v-container>
+
+    <v-container>
+      <v-date-picker
+      v-model="picker"
+      reactive
+      locale="jp-ja"
+      :day-format="date => new Date(date).getDate()"
+    ></v-date-picker>
+    </v-container>
+
+  </v-form>
+
+</template>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
+  data: {
+    newItem: ''
+    // picker: new Date().toISOString().substr(0, 10)
+  },
+  methods: {
+    addItem: function (event) {
+      alert()
+    }
+  },
   render: h => h(App)
 }).$mount('#app')
